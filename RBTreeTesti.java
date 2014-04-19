7b4b1a758b2ccd2c88a096a0eee63c16f0ad982d
unknown <tero_j_kettunen@msn.com>
2014-04-19 14:58:01 +0300

    Lisätty setSize ja jatkettu testiohjelmaa

--- RBTreeTesti.java
+++ RBTreeTesti.java
@@ -22,20 +22,28 @@ public class RBTreeTesti {
     //Testataan isEmpty();
     
     if(puu.isEmpty())
-      System.out.println("Puu on tyhjä");
+      System.out.println("Puu on tyhja");
       
     Random R = new Random();
     
-    
+    //tulostetaan testipuu
+	
+	RBTree<Integer> testipuu = testTree();
+	
+	printTree(testipuu);
     
     //Testataan add() satunnaisilla luvuilla
     
-    System.out.println("Lisätään");
+    System.out.println("Lisataan");
     for(int i = 0; i<N;i++){
       Integer x = R.nextInt(N);
       System.out.print(x + " ");
       puu.add(x);
     }
+	
+	System.out.println();
+	printTree(puu);
+	
   
     //Testataan remove()
     
@@ -58,59 +66,60 @@ public class RBTreeTesti {
       
   
   //TODO, tarkista toimiiko edes
-  private RBTree<Integer> testTree(){
+  private static RBTree<Integer> testTree(){
     
     RBTree<Integer> puu = new RBTree<Integer>();
     
-    puu.setRoot(RBTreeNode<Integer>(7,null,null,null,"b"));  
+    puu.setRoot(new RBTreeNode<Integer>(7,null,null,null,1));  
         //7 juureksi,           vanhempi, lapset null, väri musta
     
     RBTreeNode<Integer> juuri = puu.getRoot();
     
     // juuren lapset
-    juuri.setLeftChild(new RBTreeNode<Integer>(2,juuri,null,null,"r"));
-    juuri.setRightChild(new RBTreeNode<Integer>(11,juuri,null,null,"r"));
+    juuri.setLeftChild(new RBTreeNode<Integer>(2,juuri,null,null,0));
+    juuri.setRightChild(new RBTreeNode<Integer>(11,juuri,null,null,0));
     
     //vasen haara
     RBTreeNode<Integer> vh = juuri.getLeftChild();
     
-    vh.setLeftChild(new RBTreeNode<Integer>(1,vh,null,null,"b"));
-    vh.setRightChild(new RBTreeNode<Integer>(5,vh,null,null,"b"));
+    vh.setLeftChild(new RBTreeNode<Integer>(1,vh,null,null,1));
+    vh.setRightChild(new RBTreeNode<Integer>(5,vh,null,null,1));
     
     RBTreeNode<Integer> vho = vh.getRightChild();
     
-    vho.setLeftChild(new RBTreeNode<Integer>(4,vho,null,null,"r"));
+    vho.setLeftChild(new RBTreeNode<Integer>(4,vho,null,null,0));
     
     //oikea haara
     RBTreeNode<Integer> oh = juuri.getRightChild();
     
-    oh.setLeftChild(new RBTreeNode<Integer>(8,oh,null,null,"b"));
-    oh.setRightChild(new RBTreeNode<Integer>(14,oh,null,null,"b"));
+    oh.setLeftChild(new RBTreeNode<Integer>(8,oh,null,null,1));
+    oh.setRightChild(new RBTreeNode<Integer>(14,oh,null,null,1));
     
     RBTreeNode<Integer> oho = oh.getRightChild();
     
-    oho.setLeftChild(new RBTreeNode<Integer>(15,oho,null,null,"r"));
+    oho.setLeftChild(new RBTreeNode<Integer>(15,oho,null,null,0));
     
-    
+	puu.setSize(10);
+    return puu;
     
   }
   
   
-  private void printTree(RBTree puu){
+  private static void printTree(RBTree puu){
     
     if(puu.isEmpty()){
       System.out.println("Puu on tyhjä");
     }else{
       int n = puu.size();
       RBTreeNode juuri = puu.getRoot();
-      
-      System.out.println(juuri.getColor() + " | ");// juuri.getElement());
+       System.out.println();
+      System.out.println("Vari (0=red, 1= black) |  elementti");
       //System.out.println(" /    \\ ");  
       PrintTreeApu(juuri);
     }
   }
   
-  private void PrintTreeApu(RBTreeNode node){
+  private static void PrintTreeApu(RBTreeNode node){
   
     RBTreeNode vl = node.getLeftChild();
     
@@ -118,7 +127,7 @@ public class RBTreeTesti {
       PrintTreeApu(vl);
     }
     
-    System.out.println(node.getColor());
+    System.out.println(node.getColor() + " | " + node.getElement() );
     
     RBTreeNode ol = node.getRightChild();
     
